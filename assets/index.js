81b73fecde497064e05330b32c90f837656340dd
winfred kagendo <47690699+winniekagz@users.noreply.github.com>
2024-01-30 12:05:45 +0300

    create a register screen

---
+++ assets/index.js
@@ -0,0 +1,15 @@
+import FacebookIcon from './icons/facebook.svg'
+import linkedInIcon from './icons/linkedIn.png'
+import twitterImg from './icons/twitter.png'
+import googleImg from './icons/google.png'
+import FacebookImg from './icons/facebook.png'
+ 
+ 
+
+export {
+
+  linkedInIcon,
+  twitterImg,
+  googleImg,
+  FacebookImg
+}
--- screens/Register.js
+++ screens/Register.js
@@ -1,82 +1,114 @@
-import React, { useState } from 'react'
-import { Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import COLORS from '../constants/Colors'
+// Register.js
+import React, { useState } from 'react';
+import { Button, Image, Text, TextInput, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import COLORS from '../constants/Colors';
 
-import { useFormik } from 'formik';
+import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import CustomButton from '../components/Button';
 import Input from '../components/FormInputs/Input';
+import { useForm} from 'react-hook-form';
+import PasswordInput from '../components/FormInputs/PasswordInput';
+import {FacebookImg, googleImg,  linkedInIcon, twitterImg } from '../assets';
+import HorizontalLineWithName from '../components/HorizontalNameWithName'
+import PageHeader from '../components/PageHeader';
+import SocialIcon from '../components/SocialIcon';
 
-export default function Register({navigation}) {
-  const [formData,setFormData]=useState({email:'',password:''})
+export default function Register({ navigation }) {
+  const [formData, setFormData] = useState({ email: '', password: '' });
 
-  const validationSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Email is required'),
-    
+  const schema = Yup.object().shape({
+    email: Yup.string().required('Email is required').email('Invalid email'),
+    name: Yup.string().required('name is required'),
+    password: Yup.string().required('Password is required').min(8, 'Password must contain at least 8 characters'),
+    'confirmPassword': Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Passwords must match')
+    .required('Confirm Password is required'),
+
   });
 
-  const formik = useFormik({
-    initialValues: {
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
       email: '',
       password: '',
     },
-    validationSchema,
-    onSubmit: (values) => {
-      // Handle form submission
-      console.log(values);
-    },
   });
 
-  const handleChange = (event) => {
-    event.preventDefault();
-    setFormData((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  const onPressSend = (formData) => {
+    alert('test')
+    console.log('formData', formData);
   };
-  return (
-    <SafeAreaView style={{flex:1,backgroundColor:COLORS.white}}>
-    <View style={{marginHorizontal: 22, flex: 1, justifyContent: 'center' }}>
-    <Text style={{
-      fontSize:22,
-      fontWeight:'bold',
-      marginVertical:12,
-      color:COLORS.primary
-    }}>Create Account </Text>
-    <Text style={{
-      color:COLORS.black,
-      fontSize:16
-    }}
-    >Connect with your friends</Text>
 
-<Input
-            placeholder='Enter your Email'
-            placeholderTextColor={COLORS.grey}
-            mode='outlined'
-            keyboardType='email-address'
-            style={{ width: '100%', height: 48 }}
-            name='email'
-            value={formik.values.email}
-            onChangeText={formik.handleChange('email')}
-            onBlur={formik.handleBlur('email')}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <Text style={{ color: 'red' }}>{formik.errors.email}</Text>
-          ) : null}
-  <View style={{marginTop:'21px'}}>
-  <CustomButton 
-        buttonText={'Register'}
-         color={COLORS.secondary}
-         onPress={()=>navigation.navigate('login')
-        }
-         style={{
-          marginVertical:22,
-          padding:10,
-          width:'100%',
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+      <View style={{ marginHorizontal: 22, flex: 1, justifyContent: 'center' }}>
+     <PageHeader title={'Hello, Create Account'} subTitle={'First, lets create your account'}/>
+        <View>
+      <View>
+        <Input  control={control}
+        name="email"
+        label={'email'}
+        placeholder="email"
+        rules={{ required: true }}
+        errors={errors}
+        secureTextEntry
+      />
         
-         }}/>
-  </View>
+        <Input  control={control}
+        name="name"
+        label={'Full Name'}
+        placeholder="Enter Full Name"
+        rules={{ required: true }}
+        errors={errors}
+        secureTextEntry
+      />
+        <PasswordInput  control={control}
+        name="password"
+        label={'password'}
+        placeholder="Enter password"
+        rules={{ required: true }}
+        errors={errors}
+        secureTextEntry
+      />
+
+<PasswordInput  control={control}
+        name="confirmPassword"
+        label={'Confirm Password'}
+        placeholder="Enter password"
+        rules={{ required: true }}
+        errors={errors}
+        secureTextEntry
+      />
+      </View>
+
+      <View style={{  }}>
+          <CustomButton
+            buttonText={'Register'}
+            color={COLORS.secondary}
+            onPress={handleSubmit(onPressSend)}
+            style={{
+              marginVertical: 22,
+              padding: 10,
+              width: '100%',
+            }}
+          />
+        </View> 
+        <View style={{flexDirection:'row',marginVertical:16}}>
+          <Text style={{color:COLORS.grey,fontSize:16,fontWeight:'600'}}>Already have an account ?</Text>
+          <Text style={{color:COLORS.secondary,fontSize:16,fontWeight:'bold'}} onPress={()=>navigation.navigate('login')}> Login </Text>
+        </View>
+        <HorizontalLineWithName name={'OR'}/>
     </View>
-   
-   
-  </SafeAreaView>
-  )
+
+      <View style={{flexDirection:'row',gap:20,justifyContent:'center',alignItems:'center',marginVertical:16}}>
+      <SocialIcon source={FacebookImg} />
+      <SocialIcon source={googleImg}  />
+      <SocialIcon source={twitterImg}/>
+      <SocialIcon source={linkedInIcon }  /> 
+      </View>
+      </View>
+    </SafeAreaView>
+  );
 }
